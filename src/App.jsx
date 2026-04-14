8d896b4ab98b425aac2f7a9c768b09cb721c610b
namithagajawada <namithagajawada2gmail.com>
2026-04-14 21:43:26 +0530

    removed review

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skill from './components/Skill';
 import Work from './components/Work';
-import Review from './components/Review';
+//import Review from './components/Review';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -58,7 +58,7 @@ const App=()=>{
       <About />
       <Skill />
       <Work />
-      <Review />
+      
       <Contact />
       
     </main>
